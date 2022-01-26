2f9e493b06c2a911a3bec65cfebecdcac9936588
Vasil Kosturski <v.kosturski@synergysportstech.com>
2022-01-26 15:38:31 +0200

    Completed REST Proxy Producer

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -37,9 +37,11 @@ class Weather(Producer):
         #
         #
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            "weather_stream",  # TODO: Come up with a better topic name
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=1,
+            num_replicas=1
         )
 
         self.status = Weather.status.sunny
@@ -79,31 +81,30 @@ class Weather(Producer):
         # specify the Avro schemas and verify that you are using the correct Content-Type header.
         #
         #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        logger.info("weather kafka proxy integration completed")
+        resp = requests.post(
+            # TODO: What URL should be POSTed to?
+            f"{Weather.rest_proxy_url}/topics/{self.topic_name}",  # Check if this is same as topic name above
+            # TODO: What Headers need to bet set?
+            headers={"Content-Type": "application/vnd.kafka.avro.v2+json"},
+            data=json.dumps(
+                {
+                    # TODO: Provide key schema, value schema, and records
+                    'key_schema': json.dumps(Weather.key_schema),
+                    # Avro schema data has to be a string, json.dumps converts JSON into a str
+                    'value_schema': json.dumps(Weather.value_schema),
+                    "records": [
+                        {"key": {"timestamp": self.time_millis()},
+                         "value": {
+                             "temperature": self.temp,
+                             "status": self.status.name
+                         }
+                         }
+                    ]
+                }
+            )
+        )
+        resp.raise_for_status()
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
