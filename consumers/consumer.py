422317c7df004e168ca753ce8320487dd79ebfe2
Vasil Kosturski <v.kosturski@synergysportstech.com>
2022-01-27 17:50:11 +0200

    Completed consumer.py

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -37,18 +37,17 @@ class KafkaConsumer:
         #
         #
         self.broker_properties = {
-                #
-                # TODO
-                #
+            "bootstrap.servers": BROKER_URL,
+            "group.id": topic_name_pattern,
+            "default.topic.config": {"auto.offset.reset": "earliest"}
         }
 
         # TODO: Create the Consumer, using the appropriate type.
         if is_avro is True:
-            self.broker_properties["schema.registry.url"] = "http://localhost:8081"
-            #self.consumer = AvroConsumer(...)
+            self.broker_properties["schema.registry.url"] = SCHEMA_REGISTRY_URL
+            self.consumer = AvroConsumer(self.broker_properties)
         else:
-            #self.consumer = Consumer(...)
-            pass
+            self.consumer = Consumer(self.broker_properties)
 
         #
         #
@@ -56,20 +55,16 @@ class KafkaConsumer:
         # how the `on_assign` callback should be invoked.
         #
         #
-        # self.consumer.subscribe( TODO )
+        self.consumer.subscribe([self.topic_name_pattern], on_assign=self.on_assign)
 
     def on_assign(self, consumer, partitions):
         """Callback for when topic assignment takes place"""
         # TODO: If the topic is configured to use `offset_earliest` set the partition offset to
         # the beginning or earliest
-        logger.info("on_assign is incomplete - skipping")
+        logger.info("on_assign is completed")
         for partition in partitions:
-            pass
-            #
-            #
-            # TODO
-            #
-            #
+            if self.offset_earliest:
+                partition.offset = OFFSET_BEGINNING
 
         logger.info("partitions assigned for %s", self.topic_name_pattern)
         consumer.assign(partitions)
@@ -84,21 +79,27 @@ class KafkaConsumer:
 
     def _consume(self):
         """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
-        #
-        #
+
         # TODO: Poll Kafka for messages. Make sure to handle any errors or exceptions.
         # Additionally, make sure you return 1 when a message is processed, and 0 when no message
         # is retrieved.
-        #
-        #
-        logger.info("_consume is incomplete - skipping")
-        return 0
+
+        try:
+            message = self.consumer.poll(timeout=self.consume_timeout)
+        except error:
+            logger.error('Cannot poll message from {}'.format(self.topic_name_pattern))
+            return 0
+
+        if message is None:
+            return 0
+        elif message.error() is not None:
+            return 0
+        else:
+            self.message_handler(message)
+            return 1
 
 
     def close(self):
         """Cleans up any open kafka consumers"""
-        #
-        #
         # TODO: Cleanup the kafka consumer
-        #
-        #
+        self.consumer.close()
