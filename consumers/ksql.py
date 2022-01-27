e4a6bca7537b9d3f3fbb6b8aca5b4cd11cd1c433
Vasil Kosturski <v.kosturski@synergysportstech.com>
2022-01-27 16:45:51 +0200

    Completed ksql.py

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -23,14 +23,20 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC = 'turnstiles_per_station',
+    VALUE_FORMAT = 'avro',
+    KEY = 'station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT = 'json') AS
+    SELECT station_id, COUNT(station_id) AS count
+    FROM turnstile
+    GROUP BY station_id;
 """
 
 
