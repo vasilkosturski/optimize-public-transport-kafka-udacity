12737c1eaedf4fdd126c9bfc09cc73393bfe90bb
Vasil Kosturski <v.kosturski@synergysportstech.com>
2022-01-28 14:06:07 +0200

    Small fix

--- producers/connector.py
+++ producers/connector.py
@@ -25,13 +25,6 @@ def configure_connector():
     # using incrementing mode, with `stop_id` as the incrementing column name.
     # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
     # Connect should run this connector (hint: not very often!)
-        return
-
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
     logger.info("connector code not completed")
     resp = requests.post(
         KAFKA_CONNECT_URL,
