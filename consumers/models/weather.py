2b54b934980a7e27f71acf5f4989b2dc7721ffae
Vasil Kosturski <v.kosturski@synergysportstech.com>
2022-01-28 12:42:38 +0200

    Completed weather.py

--- consumers/models/weather.py
+++ consumers/models/weather.py
@@ -15,9 +15,9 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
+        logger.info("weather process_message completed")
+
         # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+        json_data = message.value()
+        self.temperature = json_data['temperature']
+        self.status = json_data['status']
